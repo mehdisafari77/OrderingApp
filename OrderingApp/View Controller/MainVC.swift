66266dfd8a19751e73f84e5a33241f81fe2abd5e
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-24 08:31:33 -0800

    added variables in detail menu vc

--- OrderingApp/View Controller/MainVC.swift
+++ OrderingApp/View Controller/MainVC.swift
@@ -7,23 +7,76 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var orderButton: UIButton!
+    @IBOutlet weak var myCollectionView: UICollectionView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
+    // MARK: - Collection View Datasource
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 
-    /*
+
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+
     }
-    */
+
+    // MARK: - Animation
+    func animateAstroDude(myImageView: UIImageView) {
+        let originalCenter = myImageView.center
+        UIView.animateKeyframes(withDuration: 1.5, delay: 0.0, animations: {
+
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.25, animations: {
+                myImageView.center.x -= 80.0
+                myImageView.center.y += 10.0
+            })
+
+            UIView.addKeyframe(withRelativeStartTime: 0.1, relativeDuration: 0.4) {
+                myImageView.transform = CGAffineTransform(rotationAngle: -.pi / 80)
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.25) {
+                myImageView.center.x -= 100.0
+                myImageView.center.y += 50.0
+                myImageView.alpha = 0.0
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.51, relativeDuration: 0.01) {
+                myImageView.transform = .identity
+                myImageView.center = CGPoint(x:  900.0, y: 100.0)
+            }
+
+            UIView.addKeyframe(withRelativeStartTime: 0.55, relativeDuration: 0.45) {
+                myImageView.center = originalCenter
+                myImageView.alpha = 1.0
+            }
+
+        }, completion: { (_) in
+        })
+    }
+
+
+    // MARK: - Action
+    @IBAction func orderButtonTapped(_ sender: Any) {
+
+    }
 
 }
+
