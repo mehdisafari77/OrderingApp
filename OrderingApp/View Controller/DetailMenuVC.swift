21d566f1ce95d38e2df7bb08cd68e485a0fd7af3
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-02-24 08:32:11 -0800

    add pizza data and custom collection view variables

--- OrderingApp/View Controller/DetailMenuVC.swift
+++ OrderingApp/View Controller/DetailMenuVC.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class DetailMenuVC: UIViewController {
-
+class DetailMenuVC: UIViewController{
+    
+    @IBOutlet weak var pizzaImageView: UIImageView!
+    @IBOutlet weak var prepTimeLabel: UILabel!
+    @IBOutlet weak var calsLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var cheeseLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dismissButton: UIButton!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        
     }
-    */
-
+    
+    @IBAction func dismissButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
+
--- OrderingApp/View/ItemCVCell.swift
+++ OrderingApp/View/ItemCVCell.swift
@@ -8,5 +8,10 @@
 import UIKit
 
 class ItemCVCell: UICollectionViewCell {
-    
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pizzaImage: UIImageView!
+    @IBOutlet weak var cheeseLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+
 }
